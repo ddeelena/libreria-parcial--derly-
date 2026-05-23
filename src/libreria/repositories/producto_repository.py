fe7b2e8694a74962b75f36c8a8ef3ca344402da7
Derly <dquejada1028@cue.edu.co>
2026-05-22 19:06:30 -0500

    REFACTOR: Requisito 1

--- src/libreria/repositories/producto_repository.py
+++ src/libreria/repositories/producto_repository.py
@@ -1,2 +1,15 @@
 class ProductoRepository:
-    pass
+    def __init__(self):
+        self.productos = []
+
+    def agregar_producto(self, producto):
+        self.productos.append(producto)
+
+    def obtener_productos(self):
+        return self.productos
+    
+    def obtener_producto_por_nombre(self, nombre):
+        for producto in self.productos:
+            if producto.nombre == nombre:
+                return producto
+        return None
--- src/libreria/services/producto_service.py
+++ src/libreria/services/producto_service.py
@@ -2,10 +2,12 @@
 from libreria.exceptions.precio_base_cero import PrecioBaseCeroException
 from libreria.models.producto import Producto
 from libreria.exceptions.valor_negativo import ValorNegativoException
+from libreria.repositories.producto_repository import ProductoRepository
 
 class ProductoService:
     def __init__(self):
         self.productos = {}
+        self.repository = ProductoRepository()
 
     def crear_producto(self, nombre, precio):
         if precio == 0:
@@ -13,7 +15,7 @@ class ProductoService:
         if precio < 0:
             raise ValorNegativoException("El precio no puede ser negativo.")
         producto = Producto(nombre, precio)
-        self.productos[nombre] = producto
+        self.repository.agregar_producto(producto)
 
     def obtener_producto(self, nombre):
-        return self.productos.get(nombre)
+        return self.repository.obtener_producto_por_nombre(nombre)
